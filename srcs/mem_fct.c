2ccbf13fd13a87caf45f1b413b062c226240ba54
Hutaillefu <hugo.taillefumier@yahoo.fr>
2019-01-06 13:10:43 +0100

    move fct

--- srcs/mem_fct.c
+++ srcs/mem_fct.c
@@ -1,1 +1,14 @@
 #include "ft_regex.h"
+
+t_pattern	*create_pattern(int (*is_match)(char *, int, t_pattern *))
+{
+	t_pattern	*pattern;
+
+	if (!(pattern = (t_pattern *)ft_memalloc(sizeof(t_pattern))))
+		return (NULL);
+	pattern->dyn_str = ft_strdup("");
+	pattern->min = 1;
+	pattern->max = 1;
+	pattern->is_match = is_match;
+	return (pattern);
+}
