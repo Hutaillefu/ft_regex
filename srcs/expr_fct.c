b12261f21165ad3352f674fdbda5e471510a8f80
Hutaillefu <hugo.taillefumier@yahoo.fr>
2019-01-06 13:46:19 +0100

    move fct

--- srcs/expr_fct.c
+++ srcs/expr_fct.c
@@ -1,1 +1,40 @@
 #include "ft_regex.h"
+
+int		collection_match(char *str, int index, t_pattern *pattern)
+{
+	char **tokens;
+	int	i;
+
+	if (!str || index < 0 || !pattern)
+		return (-1);
+
+	tokens = ft_strsplit(pattern->dyn_str, '\n');
+	i = 0;
+	while (tokens[i])
+	{
+		if (ft_strlen(tokens[i]) == 1 && str[index] == tokens[i][0])
+		{
+			return (1); // match 1 char
+		}
+		i++;
+	}
+	return (-1);
+}
+
+
+
+int		char_match(char *str, int index, t_pattern *pattern)
+{
+	int nb;
+
+	nb = index;
+	while ((pattern->dyn_str[0] == '.' ? str[nb] != '\n' : str[nb] == pattern->dyn_str[0]))
+	{
+		if (pattern->max == 1) // ?
+			return (1);
+		nb++;
+	}
+	if (nb - index == 0 && pattern->min == 1)
+		return (-1);
+	return (nb - index);	
+}
