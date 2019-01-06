50a5f73cf1327181d768ab8c62f93ef35cf49509
Hutaillefu <hugo.taillefumier@yahoo.fr>
2019-01-06 13:10:51 +0100

    move fct

--- srcs/ft_regex.c
+++ srcs/ft_regex.c
@@ -18,19 +18,6 @@ void		add_str(char **str, char *new_str)
 	free(ret);
 }
 
-t_pattern	*create_pattern(int (*is_match)(char *, int, t_pattern *))
-{
-	t_pattern	*pattern;
-
-	if (!(pattern = (t_pattern *)ft_memalloc(sizeof(t_pattern))))
-		return (NULL);
-	pattern->dyn_str = ft_strdup("");
-	pattern->min = 1;
-	pattern->max = 1;
-	pattern->is_match = is_match;
-	return (pattern);
-}
-
 int		collection_match(char *str, int index, t_pattern *pattern)
 {
 	char **tokens;
